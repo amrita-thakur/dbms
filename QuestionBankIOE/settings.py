e9cd9a88bb136e2e0ccbcbe43f9e8916c09168e5
Amrita <70787507+amrita20@users.noreply.github.com>
2020-12-22 00:05:06 +0545

    Update settings.py

--- QuestionBankIOE/settings.py
+++ QuestionBankIOE/settings.py
@@ -79,16 +79,22 @@ WSGI_APPLICATION = 'QuestionBankIOE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "ioe_question_bank_v1",
+#         "USER": "django",
+#         "PASSWORD": "django-pass",
+#         "HOST": "localhost"
+#     }
+# }
+
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "ioe_question_bank_v1",
-        "USER": "django",
-        "PASSWORD": "django-pass",
-        "HOST": "localhost"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
